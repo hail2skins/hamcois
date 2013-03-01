c771c71bef0980fde010803aef6ffcea90d04f2d
Art Mills <art@hamcois.com>
2013-02-28 22:50:53 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hamcois::Application.config.secret_key_base = '928ff4914e086129890b0573cd2a616ca639fa2c0c8aa8ce95755babb18615d255b86c64d2bf964f1677e5725a7868f4ea6b8b2deab38fe8e6cca2b211693cc2'
