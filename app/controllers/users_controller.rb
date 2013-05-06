8aa19e8fc0044556a8d3ec417e127b632a264e4a
Art Mills <art@hamcois.com>
2013-05-06 02:22:33 -0500

    Working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,7 +72,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
     def signed_in_user
