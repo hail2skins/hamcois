e34b6825c16ea1674b4bb1b130ac9b02499f8b08
Art Mills <art@hamcois.com>
2013-05-06 02:18:14 -0500

    Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  before_action :authorize
+  #before_action :authorize
   
   delegate :allow?, to: :current_permission
   helper_method :allow?
